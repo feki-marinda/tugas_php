750516ef64278983851d20f0daf9fae00ceffdd0
feki marinda <200631100063@student.ac.id>
2023-04-27 19:05:36 +0700

    mengerjakan bangun datar dan bangun ruang

---
+++ bangun_datar.php
@@ -0,0 +1,24 @@
+<?php
+echo "Menghitung Luas Bangun Datar <br>";
+$panjang  = 5;
+$lebar  = 10;
+$tinggipersegi=15;
+$luas = $panjang*$lebar;
+echo "Luas Persegi panjang= $luas";
+echo "<br>=====================<br>";
+$alas = 20;
+$tinggi = 10;
+$luassegitiga = 1/2 * $alas*$tinggi;
+echo "luas segitiga = $luassegitiga";
+echo "<br>=====================<br>";
+$jajargenjang = $alas*$tinggi;
+echo "luas jajar genjang = $jajargenjang";
+echo "<br>=====================<br>";
+$sisi = 15;
+$luaspersegi = $sisi*$sisi;
+echo "Luas Persegi = $luaspersegi";
+echo "<br>=====================<br>";
+$r=7;
+$luaslingkaran = 22/7*$r*$r;
+echo "luas lingkaran = $luaslingkaran";
+?>
---
+++ bangun_ruang.php
@@ -0,0 +1,17 @@
+<?php
+echo "menghitung Volume";
+echo "<br>=====================<br>";
+$panjang = 15;
+$lebar = 10;
+$tinggi= 5;
+$volumebalok = $panjang*$lebar*$tinggi;
+echo "volume balok = $volumebalok";
+echo "<br>=====================<br>";
+$sisi = 8;
+$volumebalok = $sisi*$sisi*$sisi;
+echo "volume kubus = $volumebalok";
+echo "<br>=====================<br>";
+$r = 7;
+$volumebola = 4/3 * 22/7 *$r*$r*$r;
+echo "volume bola = $volumebola";
+?>
