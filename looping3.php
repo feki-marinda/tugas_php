ccac6a7f3dac7c88bf04319e9e71e5c348e80f42
feki marinda <200631100063@student.ac.id>
2023-04-27 20:06:00 +0700

    menyelesaikan looping

---
+++ looping3.php
@@ -0,0 +1,8 @@
+<?php
+for($a=0;$a<=9;$a++){ // ini digunakan untuk membuat baris, terdapat 9 baris
+    for($b=0;$b<$a;$b++){ //ini digunakan untuk membuat kolom yang akan bertambah pada tiap barisnya
+        echo "$b"; //ini adalah output yang ditampilkan yaitu nilai dari $b
+    }
+    echo "<br>";
+}
+?>
